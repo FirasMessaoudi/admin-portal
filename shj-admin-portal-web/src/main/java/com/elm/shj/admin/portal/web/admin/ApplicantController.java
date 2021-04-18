0f3536619095da70609a805f0fd9fe0d1cc175a0
Ahmad Amin Flaifel <aflaifel@elm.sa>
2021-04-18 13:55:35 +0300

    applicant related classes (controller, service and repository) HAJ-1

---
+++ shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/admin/ApplicantController.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 ELM. All rights reserved.
+ */
+package com.elm.shj.admin.portal.web.admin;
+
+import com.elm.shj.admin.portal.services.applicant.ApplicantService;
+import com.elm.shj.admin.portal.web.navigation.Navigation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Main controller for applicant management pages
+ *
+ * @author ahmad flaifel
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping(Navigation.API_APPLICANTS)
+@Slf4j
+public class ApplicantController {
+
+    @Autowired
+    private ApplicantService applicantService;
+
+}
