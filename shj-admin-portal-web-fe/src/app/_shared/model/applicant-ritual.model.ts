63da1433d9a58832c3b8067c9961a035d34f6f69
Ahmad Amin Flaifel <aflaifel@elm.sa>
2021-04-21 13:25:36 +0300

    Updates HAJ-1

--- shj-admin-portal-web-fe/src/app/_shared/model/applicant-ritual.model.ts
+++ shj-admin-portal-web-fe/src/app/_shared/model/applicant-ritual.model.ts
@@ -1,9 +1,10 @@
 import {Applicant} from "@model/applicant.model";
+import {HamlahPackage} from "@model/hamlah-package.model";
 
 export class ApplicantRitual {
   id: number;
   applicant: Applicant;
-  hamlahPackageId: number;
+  hamlahPackage: HamlahPackage;
   hijriSeason: number;
   dateStartGregorian: Date;
   dateEndGregorian: Date;
