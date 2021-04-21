3db12a480d2fae07e04aa30b9562844beb7c4414
Ahmad Amin Flaifel <aflaifel@elm.sa>
2021-04-21 16:26:30 +0300

    card service HAJ-1

---
+++ shj-admin-portal-web-fe/src/app/_core/services/card/card.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  list(pageNumber: any): Observable<any> {
+    let params = new HttpParams().set('page', pageNumber);
+    return this.http.get<any>("/core/api/cards/list", {params: params});
+  }
+}
