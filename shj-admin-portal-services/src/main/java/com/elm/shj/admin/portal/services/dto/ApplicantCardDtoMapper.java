69c75b966b366d87577046bb9541b1b4fbe158a8
Ahmad Amin Flaifel <aflaifel@elm.sa>
2021-04-22 14:31:34 +0300

    Fix id number column sql data type and add applicant card mapper HAJ-1

---
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/ApplicantCardDtoMapper.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021 ELM. All rights reserved.
+ */
+package com.elm.shj.admin.portal.services.dto;
+
+import com.elm.dcc.foundation.commons.core.mapper.IGenericMapper;
+import com.elm.shj.admin.portal.orm.entity.JpaApplicantCard;
+import org.mapstruct.Mapper;
+
+/**
+ * Mapper for {@link ApplicantCardDto} class
+ *
+ * @author ahmad flaifel
+ * @since 1.0.0
+ */
+@Mapper(componentModel = "spring")
+public abstract class ApplicantCardDtoMapper implements IGenericMapper<ApplicantCardDto, JpaApplicantCard> {
+}
