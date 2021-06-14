125e57c6f8dfe32d93088bec3bb11c51c2fd0670
Ahmad Amin Flaifel <aflaifel@elm.sa>
2021-06-14 13:34:32 +0300

    update and add user authorities constants and database HAJ-1

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/AuthorityConstants.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/AuthorityConstants.java
@@ -25,6 +25,20 @@ public final class AuthorityConstants {
     public static final String CHANGE_ROLE_STATUS = "CHANGE_ROLE_STATUS";
     public static final String RESET_USER_PASSWORD = "RESET_USER_PASSWORD";
 
+    //Printing Request Management
+    public static final String PRINTING_REQUEST_MANAGEMENT = "PRINTING_REQUEST_MANAGEMENT";
+    public static final String VIEW_PRINTING_REQUEST_DETAILS = "VIEW_PRINTING_REQUEST_DETAILS";
+    public static final String ADD_PRINTING_REQUEST = "ADD_PRINTING_REQUEST";
+
+    //Card Management
+    public static final String CARD_MANAGEMENT = "CARD_MANAGEMENT";
+    public static final String VIEW_CARD_DETAILS = "VIEW_CARD_DETAILS";
+    public static final String UPDATE_CARD = "UPDATE_CARD";
+    public static final String ACTIVATE_CARD = "ACTIVATE_CARD";
+    public static final String CANCEL_CARD = "CANCEL_CARD";
+    public static final String SUSPEND_CARD = "SUSPEND_CARD";
+    public static final String REISSUE_CARD = "REISSUE_CARD";
+    public static final String ADD_CARD = "ADD_CARD";
 
     private AuthorityConstants() {
         // creating instances is not allowed
