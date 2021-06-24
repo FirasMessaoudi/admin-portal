21c866ad8770d43b44b9b2993b0dc160ed514f66
Ahmad Amin Flaifel <aflaifel@elm.sa>
2021-06-24 13:20:54 +0300

    remove @PreAuthorized annotation for all lookups, any authenticated user can access the lookup data. HAJ-1

--- shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/lookup/LookupController.java
+++ shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/lookup/LookupController.java
@@ -9,7 +9,6 @@ import com.elm.shj.admin.portal.web.navigation.Navigation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,62 +39,53 @@ public class LookupController {
     private final PrintBatchTypeLookupService printBatchTypeLookupService;
 
     @GetMapping("/authority/list/parent")
-    @PreAuthorize("hasAuthority('" + AuthorityConstants.ROLE_MANAGEMENT + "')")
     public List<AuthorityLookupDto> listParentAuthorities(Authentication authentication) {
         return authorityLookupService.findAllParentAuthorities();
     }
 
     @GetMapping("/ritual-type/list")
-    @PreAuthorize("hasAuthority('" + AuthorityConstants.CARD_MANAGEMENT + "')")
     public List<RitualTypeLookupDto> listRitualTypes(Authentication authentication) {
         log.debug("list ritual types...");
         return ritualTypeLookupService.findAll();
     }
 
     @GetMapping("/card-status/list")
-    @PreAuthorize("hasAuthority('" + AuthorityConstants.CARD_MANAGEMENT + "')")
     public List<CardStatusLookupDto> listCardStatuses(Authentication authentication) {
         log.debug("list card statuses...");
         return cardStatusLookupService.findAll();
     }
 
     @GetMapping("/relative-relationship/list")
-    @PreAuthorize("hasAuthority('" + AuthorityConstants.CARD_MANAGEMENT + "')")
     public List<RelativeRelationshipLookupDto> listRelativeRelationships(Authentication authentication) {
         log.debug("list relative relationships...");
         return relativeRelationshipLookupService.findAll();
     }
 
     @GetMapping("/marital-status/list")
-    @PreAuthorize("hasAuthority('" + AuthorityConstants.CARD_MANAGEMENT + "')")
     public List<MaritalStatusLookupDto> listMaritalStatuses(Authentication authentication) {
         log.debug("list marital statuses...");
         return maritalStatusLookupService.findAll();
     }
 
     @GetMapping("/country/list")
-    @PreAuthorize("hasAnyAuthority('" + AuthorityConstants.CARD_MANAGEMENT + "', '" + AuthorityConstants.PRINTING_REQUEST_MANAGEMENT + "')")
     public List<CountryLookupDto> listCountries(Authentication authentication) {
         log.debug("list countries...");
         return countryLookupService.findAll();
     }
 
     @GetMapping("/health-special-needs/list")
-    @PreAuthorize("hasAuthority('" + AuthorityConstants.CARD_MANAGEMENT + "')")
     public List<HealthSpecialNeedsTypeLookupDto> listHealthSpecialNeeds(Authentication authentication) {
         log.debug("list health special needs...");
         return healthSpecialNeedsLookupService.findAll();
     }
 
     @GetMapping("/print-request-status/list")
-    @PreAuthorize("hasAuthority('" + AuthorityConstants.PRINTING_REQUEST_MANAGEMENT + "')")
     public List<PrintRequestStatusLookupDto> listPrintRequestStatuses(Authentication authentication) {
         log.debug("list print request statuses...");
         return printRequestStatusLookupService.findAll();
     }
 
     @GetMapping("/print-batch-type/list")
-    @PreAuthorize("hasAuthority('" + AuthorityConstants.PRINTING_REQUEST_MANAGEMENT + "')")
     public List<PrintBatchTypeLookupDto> listPrintRequestBatchTypes(Authentication authentication) {
         log.debug("list print batch types...");
         return printBatchTypeLookupService.findAll();
