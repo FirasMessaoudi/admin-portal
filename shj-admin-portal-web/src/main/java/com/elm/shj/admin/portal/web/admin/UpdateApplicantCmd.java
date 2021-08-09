20a42ccac4a5ee8e56ddae050d150024a73dac0a
ahali <ahali@elm.sa>
2021-08-09 18:19:12 +0300

    as an applicant i want to change my user account contacts ( Mobile , Email) HAJ-134

--- shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/admin/UpdateApplicantCmd.java
+++ shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/admin/UpdateApplicantCmd.java
@@ -5,6 +5,7 @@ package com.elm.shj.admin.portal.web.admin;
 
 import com.elm.shj.admin.portal.services.data.validators.NullOrNotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
@@ -33,6 +34,8 @@ public class UpdateApplicantCmd implements Serializable {
     @NotNull
     @Size(min = 5, max = 20)
     private String mobileNumber;
-
+    @Length(max = 20)
+    private String countryCode;
     private int dateOfBirthHijri;
+
 }
