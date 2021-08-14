99f191940a2d0ead30864d9f5d2126ce6c89638e
Ahmad Amin Flaifel <aflaifel@elm.sa>
2021-08-14 14:34:54 +0300

    Secure integration web service calls and update audit log table script HAJ-340

--- src/main/sql/1.1.0/shj-admin-portal-alter-schema.sql
+++ src/main/sql/1.1.0/shj-admin-portal-alter-schema.sql
@@ -1,11 +1,11 @@
 USE shc_portal
 GO
-
-ALTER TABLE shc_portal.shc_applicant_health
-    ADD applicant_ritual_id int;
+ALTER TABLE shc_portal.shc_applicant_health ADD applicant_ritual_id int;
 GO
 
-ALTER TABLE shc_portal.shc_applicant_health
-    ADD CONSTRAINT fk_applicant_health_applicant_ritual
-        FOREIGN KEY (applicant_ritual_id) REFERENCES shc_portal.shc_applicant_ritual (id);
+ALTER TABLE shc_portal.shc_applicant_health ADD CONSTRAINT fk_applicant_health_applicant_ritual FOREIGN KEY (applicant_ritual_id) REFERENCES shc_portal.shc_applicant_ritual (id);
+GO
+
+-- update audit log table to handle big number for user id
+ALTER TABLE shc_portal.shc_audit_log ALTER COLUMN user_id_number bigint;
 GO
---
+++ src/main/sql/1.1.0/shj-admin-portal-data.sql
@@ -0,0 +1,21 @@
+USE shc_portal
+-- insert integration web service user
+SET IDENTITY_INSERT shc_portal.shc_user ON;
+insert into shc_portal.shc_user (id, nin, gender, mobile_number, date_of_birth_gregorian, password_hash, first_name, family_name, number_of_tries, activated, deleted, creation_date)
+values (2, 9366014457, 'M', 512345678, convert(date, '01/01/1970', 103), '$2a$10$A81/FuMFJWcxaJhUcL8isuVeKKa.hk7GVzTVTyf7xe/XoMVWuKckK', 'web', 'service', 0, 'true', 'false', current_timestamp);
+SET IDENTITY_INSERT shc_portal.shc_user OFF;
+GO
+-- insert integration web service user role and authority
+SET IDENTITY_INSERT shc_portal.shc_role ON;
+INSERT INTO shc_portal.shc_role(id, label_ar, label_en, deleted, activated) VALUES (13, N'مستخدم خارجي', 'WS User', 0, 1);
+SET IDENTITY_INSERT shc_portal.shc_role OFF;
+GO
+INSERT INTO shc_portal.shc_user_role(user_id, role_id, is_main_role) VALUES (2, 13, 1);
+GO
+SET IDENTITY_INSERT shc_portal.shc_authority_lk ON;
+INSERT INTO shc_portal.shc_authority_lk(id, label_ar, label_en, code, parent_id)
+VALUES (34, N'طلب خدمة ربط', 'Integration Web Service Call', 'INTEGRATION_WEB_SERVICE_CALL', NULL);
+SET IDENTITY_INSERT shc_portal.shc_authority_lk OFF;
+GO
+INSERT INTO shc_portal.shc_role_authority(role_id, authority_id) VALUES (13, 34);
+GO
