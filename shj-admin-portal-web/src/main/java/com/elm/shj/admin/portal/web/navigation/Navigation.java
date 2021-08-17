4463145931c4a2033a88100f73589e46881c397e
Ahmad Amin Flaifel <aflaifel@elm.sa>
2021-08-17 09:36:04 +0300

    remove applicant lookup controller as its methods were moved to the integration controller HAJ-340

--- shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/navigation/Navigation.java
+++ shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/navigation/Navigation.java
@@ -45,8 +45,6 @@ public class Navigation {
 	public static final String API_USERS_RESET_PWRD = API_USERS+"/reset-password";
 	// Lookup
 	public static final String API_LOOKUP = "/api/lookup";
-	// Lookup for the applicant portal
-	public static final String API_APPLICANT_LOOKUP = "/api/applicant/lookup";
 	// Registration
 	public static final String API_REGISTRATION = "/api/register";
 	// Integration
