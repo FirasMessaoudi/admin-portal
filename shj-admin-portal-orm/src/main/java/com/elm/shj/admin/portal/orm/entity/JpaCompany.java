b576ad49175820826fcb4a11b926158e0800381d
salzoubi <salzoubi@elm.sa>
2021-09-09 16:42:00 +0300

    HAJ-1 create entities for company, company ritual season and company staff

---
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/entity/JpaCompany.java
@@ -0,0 +1,86 @@
+package com.elm.shj.admin.portal.orm.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+/**
+ * @author salzoubi
+ * @version 1.0.0
+ * **/
+@Entity
+@Table(name = "shc_company")
+@NamedQuery(name = "JpaCompany.findAll", query = "SELECT c FROM JpaCompany c")
+@Data
+@NoArgsConstructor
+public class JpaCompany implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false)
+    private long id;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "label_ar", nullable = false)
+    private String labelAr;
+
+    @Column(name = "label_en", nullable = false)
+    private String labelEn;
+
+    @Column(name = "mission_id", nullable = false)
+    private int missionId;
+
+    @Column(name = "contact_number", nullable = false)
+    private String contactNumber;
+
+    @Column(name = "website")
+    private String website;
+
+    @Column(name = "accreditation_organization", nullable = false)
+    private String accreditationOrganization;
+
+    @Column(name = "accreditation_number", nullable = false)
+    private String accreditationNumber;
+
+    @Column(name = "accreditation_date", nullable = false)
+    private Date accreditationDate;
+
+    @Column(name = "accreditation_expiry", nullable = false)
+    private Date accreditationExpiry;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "creation_date", nullable = false)
+    private Date creationDate;
+
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @OneToMany(cascade = CascadeType.PERSIST, orphanRemoval = true, mappedBy = "company")
+    private Set<JpaCompanyStaff> companyStaff;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @OneToMany(cascade = CascadeType.PERSIST, orphanRemoval = true, mappedBy = "company")
+    private Set<JpaCompanyRitualSeason> companyRitualSeasons;
+
+    @PrePersist
+    public void prePersist() {
+        creationDate = new Date();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updateDate = new Date();
+    }
+
+}
---
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/entity/JpaCompanyRitualSeason.java
@@ -0,0 +1,71 @@
+package com.elm.shj.admin.portal.orm.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author salzoubi
+ * @version 1.0.0
+ * **/
+@Entity
+@Table(name = "shc_company_ritual_season")
+@NamedQuery(name = "JpaCompanyRitualSeason.findAll", query = "SELECT j FROM JpaCompanyRitualSeason j")
+@Data
+@NoArgsConstructor
+public class JpaCompanyRitualSeason implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", nullable = false)
+    private JpaCompany companyId;
+
+    //TODO to be foregin key
+    /*@ManyToOne
+    @JoinColumn(name = "ritual_season_id",nullable = false)
+    private JpaRitualSeason ritualSeasonId;*/
+
+
+    @Column(name = "season_start", nullable = false)
+    private Date seasonStart;
+
+    @Column(name = "season_end", nullable = false)
+    private Date seasonEnd;
+
+    @Column(name = "total_quota")
+    private int totalQuota;
+
+    @Column(name = "air_quota")
+    private int airQuota;
+
+    @Column(name = "sea_quota")
+    private int seaQuota;
+
+    @Column(name = "land_quota")
+    private int landQuota;
+
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    @PrePersist
+    public void prePersist() {
+        creationDate = new Date();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updateDate = new Date();
+    }
+
+
+}
