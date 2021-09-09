c77fee3a7d3d9c77c957485fd6c708704760d947
ahmelsayed <ahmelsayed@elm.sa>
2021-09-09 16:15:39 +0300

    Story HAJ-1: add entities for this table ( shc_portal.shc_applicant_package,   shc_portal.shc_company_season_package,   shc_portal.shc_ritual_season ,   shc_portal.shc_package_catering)

---
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/entity/JpaApplicantPackage.java
@@ -0,0 +1,54 @@
+package com.elm.shj.admin.portal.orm.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * The persistent class for the shc_applicant_package database table.
+ *
+ * @author ahmed elsayed
+ * @since 1.1.0
+ */
+@Entity
+@Table(name = "shc_applicant_package")
+@NamedQuery(name = "JpaApplicantPackage.findAll", query = "SELECT j FROM JpaApplicantPackage j")
+@Data
+@NoArgsConstructor
+public class JpaApplicantPackage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false)
+    private long id;
+
+    @Column(name = "applicant_uin", nullable = false)
+    private long applicantUin;
+
+    @Column(name = "creation_date", nullable = false)
+    private Date creationDate;
+
+    @Column(name = "UPDATE_DATE")
+    private Date updateDate;
+
+//    @ManyToOne
+//    @JoinColumn(name = "ritual_package_id")
+//    private JpaRitualPackage ritualPackage;
+
+//    @LazyCollection(LazyCollectionOption.TRUE)
+//    @OneToMany(cascade = CascadeType.PERSIST, orphanRemoval = true, mappedBy = "applicantPackage")
+//    private List<JpaApplicantPackageCatering> applicantPackageCaterings;
+
+//    @LazyCollection(LazyCollectionOption.TRUE)
+//    @OneToMany(cascade = CascadeType.PERSIST, orphanRemoval = true, mappedBy = "applicantPackage")
+//    private List<JpaApplicantPackageTransportation> applicantPackageTransportations;
+
+//    @LazyCollection(LazyCollectionOption.TRUE)
+//    @OneToMany(cascade = CascadeType.PERSIST, orphanRemoval = true, mappedBy = "applicantPackage")
+//    private List<JpaApplicantPackageHousing> applicantPackageHousings;
+}
---
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/entity/JpaPackageCatering.java
@@ -0,0 +1,59 @@
+package com.elm.shj.admin.portal.orm.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * The persistent class for the shc_package_catering database table.
+ *
+ * @author ahmed elsayed
+ * @since 1.1.0
+ */
+@Entity
+@Table(name = "shc_package_catering")
+@NamedQuery(name = "JpaPackageCatering.findAll", query = "SELECT j FROM JpaPackageCatering j")
+@Data
+@NoArgsConstructor
+public class JpaPackageCatering {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false)
+    private long id;
+
+    @Column(name = "meal_code", length = 50)
+    private String mealCode;
+
+    @Column(name = "meal_time", nullable = false)
+    private int mealTime;
+
+    @Column(name = "meal_description", nullable = false, length = 256)
+    private String mealDescription;
+
+    @Column(name = "type", length = 45)
+    private String type;
+
+    @Column(name = "description_ar", length = 250)
+    private String descriptionAr;
+
+    @Column(name = "description_en", length = 125)
+    private String descriptionEn;
+
+    @Column(name = "creation_date", nullable = false)
+    private Date creationDate;
+
+    @Column(name = "UPDATE_DATE")
+    private Date updateDate;
+
+
+//    @ManyToOne
+//    @JoinColumn(name = "package_housing_id")
+//    private JpaPackageHousing packageHousing;
+
+//    @LazyCollection(LazyCollectionOption.TRUE)
+//    @OneToMany(cascade = CascadeType.PERSIST, orphanRemoval = true, mappedBy = "packageCatering")
+//    private List<JpaApplicantPackageCatering> applicantPackageCaterings;
+}
