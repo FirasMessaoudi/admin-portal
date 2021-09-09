377a70763654bdce2a528a5231403e96339642c1
f.messaoudi <firas.messaoudi@sofrecom.com>
2021-09-09 14:04:50 +0100

    applicant package catering , applicant package house and applicant package transportation HAJ-1

---
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/entity/JpaApplicantPackageHousing.java
@@ -0,0 +1,53 @@
+package com.elm.shj.admin.portal.orm.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * the persistent class for the shc_applicant_package_housing table
+ *
+ * @author firas messaoudi
+ * @since 1.0.0
+ */
+@Entity
+@Table(name = "shc_applicant_package_housing")
+@NamedQuery(name = "JpaApplicantPackageHousing.findAll", query = "SELECT j FROM JpaApplicantPackageHousing j")
+@Data
+@NoArgsConstructor
+public class JpaApplicantPackageHousing implements Serializable {
+
+    private static final long serialVersionUID = 4952018589817964421L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true, nullable = false)
+    private long id;
+
+    @Column(name = "room_number")
+    private String roomNumber;
+
+    @Column(name = "bed_number")
+    private String bedNumber;
+
+
+    @Column(name = "creation_date", nullable = false)
+    private Date creationDate;
+
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    @PrePersist
+    public void prePersist() {
+        creationDate = new Date();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updateDate = new Date();
+    }
+
+}
