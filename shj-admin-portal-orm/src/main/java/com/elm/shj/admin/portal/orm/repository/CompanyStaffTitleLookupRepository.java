d241141ca533cd2cac043f9fcb6d77392cd29f18
Slim Ben Hadj <slim.benhadj@sofrecom.com>
2021-09-12 12:50:34 +0100

    Add lookup JPA repositories HAJ-1

---
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/CompanyStaffTitleLookupRepository.java
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (c) 2021 ELM. All rights reserved.
+ */
+package com.elm.shj.admin.portal.orm.repository;
+
+import com.elm.shj.admin.portal.orm.entity.JpaCompanyStaffTitleLookup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository for company staff title lookup table.
+ *
+ * @author Slim Ben Hadj
+ * @since 1.1.0
+ */
+public interface CompanyStaffTitleLookupRepository extends JpaRepository<JpaCompanyStaffTitleLookup, Long> {
+}
