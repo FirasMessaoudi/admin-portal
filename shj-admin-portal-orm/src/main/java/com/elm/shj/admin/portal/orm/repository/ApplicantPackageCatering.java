43857251e6aa5e6ce6cd8a22f740d754b4726fcb
f.messaoudi <firas.messaoudi@sofrecom.com>
2021-09-12 12:08:47 +0100

    repositories, dtos and mapper HAJ-1

---
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/ApplicantPackageCatering.java
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (c) 2021 ELM. All rights reserved.
+ */
+package com.elm.shj.admin.portal.orm.repository;
+
+import com.elm.shj.admin.portal.orm.entity.JpaApplicantPackageCatering;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository for applicant package catering
+ *
+ * @author firas messaoudi
+ * @since 1.1.0
+ */
+public interface ApplicantPackageCatering extends JpaRepository<JpaApplicantPackageCatering, Long> {
+}
---
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/ApplicantPackageHousingDtoMapper.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021 ELM. All rights reserved.
+ */
+package com.elm.shj.admin.portal.services.dto;
+
+
+import com.elm.dcc.foundation.commons.core.mapper.IGenericMapper;
+import com.elm.shj.admin.portal.orm.entity.JpaApplicantPackageHousing;
+
+/**
+ * Mapper for {@link ApplicantPackageHousingDto } class
+ *
+ * @author firas messaoudi
+ * @since 1.1.0
+ */
+
+public abstract class ApplicantPackageHousingDtoMapper implements IGenericMapper<ApplicantPackageHousingDto, JpaApplicantPackageHousing> {
+}
