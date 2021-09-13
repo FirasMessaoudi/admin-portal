9589e96c8bc8c7170c2e4892af3520b17bbf6d13
Slim Ben Hadj <slim.benhadj@sofrecom.com>
2021-09-13 08:58:26 +0100

    Add dto classes related to lookup tables HAJ-1

---
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/CompanyRitualStepLookupDto.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 ELM. All rights reserved.
+ */
+package com.elm.shj.admin.portal.services.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Dto class for the company ritual step domain.
+ *
+ * @author Slim Ben Hadj
+ * @since 1.1.0
+ */
+@NoArgsConstructor
+@Data
+public class CompanyRitualStepLookupDto implements Serializable {
+
+    private static final long serialVersionUID = -5358244881018936417L;
+
+    private long id;
+    private String code;
+    private String lang;
+    private String label;
+    private Date creationDate;
+}
---
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/PackageTypeLookupDto.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 ELM. All rights reserved.
+ */
+package com.elm.shj.admin.portal.services.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Dto class for the package type domain.
+ *
+ * @author Slim Ben Hadj
+ * @since 1.1.0
+ */
+@NoArgsConstructor
+@Data
+public class PackageTypeLookupDto implements Serializable {
+
+    private static final long serialVersionUID = -3497963012559412824L;
+
+    private long id;
+    private String code;
+    private String lang;
+    private String label;
+    private Date creationDate;
+}
