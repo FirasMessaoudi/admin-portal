b46acc686f0888daa9d16e0d5faca9dee65621bc
jaafer.jarray <jaafer.jarray@sofrecom.com>
2021-09-16 11:01:35 +0100

    HAJ-345 display employees data in admin portal card details

--- shj-admin-portal-web-fe/src/app/_shared/model/card.model.ts
+++ shj-admin-portal-web-fe/src/app/_shared/model/card.model.ts
@@ -1,10 +1,12 @@
 import {ApplicantRitual} from "@model/applicant-ritual.model";
 import {CompanyRitualMainDataStep} from "@model/company-ritual-step";
+import { groupeLeader } from "./groupe-leader.model";
 
 export class ApplicantCard {
   id: number;
   applicantRitual: ApplicantRitual;
   companyRitualSteps: CompanyRitualMainDataStep[];
+  groupLeaders: groupeLeader[];
   referenceNumber: number;
   batchId: number;
   statusCode: any;
