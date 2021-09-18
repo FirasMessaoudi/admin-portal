5d7f810e4befa0a5d7a3ee617f8f96a3c265665f
salzoubi <salzoubi@elm.sa>
2021-09-18 21:56:23 +0300

    HAJ-1 add the missing dependancy

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/CompanyStaffTitleLookupDtoMapper.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/CompanyStaffTitleLookupDtoMapper.java
@@ -5,12 +5,14 @@ package com.elm.shj.admin.portal.services.dto;
 
 import com.elm.dcc.foundation.commons.core.mapper.IGenericMapper;
 import com.elm.shj.admin.portal.orm.entity.JpaCompanyStaffTitleLookup;
+import org.mapstruct.Mapper;
 
 /**
- * Mapper for {@link CompanyRitualStepLookupDto} class
+ * Mapper for {@link CompanyStaffTitleLookupDto} class
  *
  * @author Slim Ben Hadj
  * @since 1.1.0
  */
+@Mapper(componentModel = "spring")
 public abstract class CompanyStaffTitleLookupDtoMapper implements IGenericMapper<CompanyStaffTitleLookupDto, JpaCompanyStaffTitleLookup> {
 }
