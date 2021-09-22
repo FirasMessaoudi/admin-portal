00292617c8da5d5231ece5453fd1eb4a9cf33e5d
jaafer.jarray <jaafer.jarray@sofrecom.com>
2021-09-22 01:06:28 +0100

    HAJ-345 separate group leaders data in new component

---
+++ shj-admin-portal-web-fe/src/app/pages/card-management/card-details/group-leaders/group-leaders.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input} from "@angular/core";
+import {Lookup} from "@model/lookup.model";
+import {LookupService} from "@core/utilities/lookup.service";
+import { groupeLeader } from "@app/_shared/model/groupe-leader.model";
+import {TranslateService} from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-group-leaders',
+  templateUrl: './group-leaders.component.html',
+  styleUrls: ['./group-leaders.component.scss']
+})
+export class GroupLeadersComponent {
+
+  @Input('applicantGroupLeaders')  groupLeaders: groupeLeader[]
+  @Input('groupLeadersTitle') groupLeaderTitleLookups: Lookup[]
+  lookupService: LookupService
+
+  constructor(lookupsService: LookupService, private translate: TranslateService,) {
+    this.lookupService = lookupsService;
+  }
+
+
+
+
+}
