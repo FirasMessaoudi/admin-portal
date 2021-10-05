48dce264b2c15b143ce534267265f12f2bd85b06
ahmelsayed <ahmelsayed@elm.sa>
2021-10-05 13:22:37 +0300

    Story HAJ-604: Create Notification List front end

--- shj-admin-portal-web-fe/src/app/pages/notification-management/notification-list/notification-list.component.ts
+++ shj-admin-portal-web-fe/src/app/pages/notification-management/notification-list/notification-list.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {EAuthority, Page} from "@shared/model";
+import {AuthenticationService} from "@core/services";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-notification-list',
@@ -7,9 +11,50 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NotificationListComponent implements OnInit {
 
-  constructor() { }
+  public isSearchbarCollapsed = true;
+  pageArray: Array<number>;
+  page: Page;
+  searchForm: FormGroup;
 
-  ngOnInit(): void {
+  private listSubscription: Subscription;
+  private searchSubscription: Subscription;
+
+  constructor(private authenticationService: AuthenticationService,
+              private formBuilder: FormBuilder) {
   }
 
+  ngOnInit(): void {
+
+    this.initForm();
+    // this.loadLookups();
+    // this.loadPage(0);
+  }
+
+  private initForm(): void {
+    this.searchForm = this.formBuilder.group({
+      statusCode: [null],
+      description: ['']
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.listSubscription) {
+      this.listSubscription.unsubscribe();
+    }
+    if (this.searchSubscription) {
+      this.searchSubscription.unsubscribe();
+    }
+  }
+
+  get canSeeNotificationList(): boolean {
+    return this.authenticationService.hasAuthority(EAuthority.NOTIFICATION_MANAGEMENT);
+  }
+
+  search(): void {
+
+  }
+
+  loadPage(page: number) {
+
+  }
 }
