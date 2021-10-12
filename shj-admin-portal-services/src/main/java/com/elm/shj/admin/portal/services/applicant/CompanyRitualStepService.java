8af4c775357d19b9280380bc8b5b4bd44783b4e3
Firas MESSAOUDI <f.messaoudi@sofrecom-tunisie.com>
2021-10-12 08:27:08 +0100

    HAJ-1 refactoring companyRitualStep

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/applicant/CompanyRitualStepService.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/applicant/CompanyRitualStepService.java
@@ -6,7 +6,6 @@ package com.elm.shj.admin.portal.services.applicant;
 import com.elm.shj.admin.portal.orm.entity.JpaCompanyRitualStep;
 import com.elm.shj.admin.portal.orm.repository.CompanyRitualStepRepository;
 import com.elm.shj.admin.portal.services.dto.CompanyRitualStepDto;
-import com.elm.shj.admin.portal.services.dto.CompanyRitualStepMainDataDto;
 import com.elm.shj.admin.portal.services.generic.GenericService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
