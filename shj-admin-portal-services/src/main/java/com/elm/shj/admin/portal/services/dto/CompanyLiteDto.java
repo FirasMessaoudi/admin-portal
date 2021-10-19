b9665d9e517e0edbdbcb3f1e34a6dc3284e4fa8c
jaafer.jarray <jaafer.jarray@sofrecom.com>
2021-10-19 11:55:04 +0100

    HAJ-630: adding company location

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/CompanyLiteDto.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/CompanyLiteDto.java
@@ -27,4 +27,8 @@ public class CompanyLiteDto implements Serializable {
     private String labelEn;
 
     private String contactNumber;
+
+    private double locationLat;
+
+    private double locationLng;
 }
