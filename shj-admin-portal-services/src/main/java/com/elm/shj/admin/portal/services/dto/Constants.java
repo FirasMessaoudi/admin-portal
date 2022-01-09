861bfb287ef3fa4ebabbe0f144f19d1efabbe660
rimtiaz <rimtiaz@elm.sa>
2022-01-10 02:04:37 +0300

    HAJ-1071 - Add incident api - valid error response not shown with invalid geo cordinates

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/Constants.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/Constants.java
@@ -12,6 +12,12 @@ package com.elm.shj.admin.portal.services.dto;
 public class Constants {
 
     public static final long SYSTEM_USER_ID_NUMBER = -1;
+    public static final String VIDEO_TYPE_FLV = "video/x-flv";
+    public static final String VIDEO_TYPE_MP4 = "video/mp4";
+    public static final String VIDEO_TYPE_3GP = "video/3gpp";
+    public static final String VIDEO_TYPE_MOV = "video/quicktime";
+    public static final String VIDEO_TYPE_AVI = "video/x-msvideo";
+    public static final String VIDEO_TYPE_WMV = "video/x-ms-wmv";
 
     private Constants() {
         // hide public constructor
