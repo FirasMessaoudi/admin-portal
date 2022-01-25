93974157a7781c593c5e4133a015b0c12744f041
Slim Ben Hadj <slim.benhadj@sofrecom.com>
2022-01-25 21:36:02 +0100

    HAJ-1416: Fix total number of male applicants for previous season

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dashboard/DashboardService.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dashboard/DashboardService.java
@@ -15,10 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.*;
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
