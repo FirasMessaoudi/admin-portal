6c174bc27e5a1651daadb36ee8d9de07871984e7
salzoubi <salzoubi@elm.sa>
2022-02-13 12:38:48 +0300

    HAJ-1486 as staff ( normal staff - group leader ) i want to search for person ( applicant or staff ) using UIN - Id Number ( NIN - Iqama - Passport ) - QR )

--- shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/ApplicantLiteRepository.java
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/ApplicantLiteRepository.java
@@ -3,6 +3,7 @@
  */
 package com.elm.shj.admin.portal.orm.repository;
 
+import com.elm.shj.admin.portal.orm.entity.ApplicantStaffVO;
 import com.elm.shj.admin.portal.orm.entity.JpaApplicantLite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -21,4 +22,43 @@ public interface ApplicantLiteRepository extends JpaRepository<JpaApplicantLite,
 
     @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM JpaApplicantLite a INNER JOIN JpaApplicantDigitalId adi ON adi.applicantId = a.id WHERE adi.uin = :uin")
     boolean existsByUin(@Param("uin") String uin);
+
+    @Query(value = "SELECT NEW com.elm.shj.admin.portal.orm.entity.ApplicantStaffVO ( applicantDigitalId.uin, applicant.fullNameEn, applicant.fullNameAr, " +
+            "ritualSeason.ritualTypeCode, card.statusCode, applicant.photo, " +
+            "applicantPackage.id, groupLeaderDigitalId.suin, groupLeader.mobileNumber,groupLeader.mobileNumberIntl,company.labelEn,company.labelAr  ) " +
+            "FROM JpaApplicantCard card " +
+            "INNER JOIN card.applicantRitual ritual  " +
+            "INNER JOIN ritual.applicant applicant " +
+            "INNER JOIN ritual.applicantPackage applicantPackage "+
+            "INNER JOIN applicant.digitalIds applicantDigitalId "+
+            "INNER JOIN JpaGroupApplicantList groupApplicantList on groupApplicantList.applicantUin = applicantDigitalId.uin " +
+            "INNER JOIN groupApplicantList.applicantGroup applicantGroup " +
+            "INNER JOIN applicantGroup.groupLeader groupLeader " +
+            "INNER JOIN groupLeader.digitalIds groupLeaderDigitalId " +
+            "INNER JOIN applicantGroup.companyRitualSeason companyRitualSeason " +
+            "INNER JOIN companyRitualSeason.ritualSeason ritualSeason " +
+            "INNER JOIN companyRitualSeason.company company " +
+            "WHERE applicant.idNumberOriginal =:idNumber OR " +
+            "applicant.passportNumber =:idNumber"
+            )
+    ApplicantStaffVO findApplicantRitualByIdNumber(@Param("idNumber") String idNumber);
+
+    @Query(value = "SELECT NEW com.elm.shj.admin.portal.orm.entity.ApplicantStaffVO ( applicantDigitalId.uin, applicant.fullNameEn, applicant.fullNameAr, " +
+            "ritualSeason.ritualTypeCode, card.statusCode, applicant.photo, " +
+            "applicantPackage.id, groupLeaderDigitalId.suin, groupLeader.mobileNumber,groupLeader.mobileNumberIntl,company.labelEn,company.labelAr  ) " +
+            "FROM JpaApplicantCard card " +
+            "INNER JOIN card.applicantRitual ritual  " +
+            "INNER JOIN ritual.applicant applicant " +
+            "INNER JOIN ritual.applicantPackage applicantPackage "+
+            "INNER JOIN applicant.digitalIds applicantDigitalId "+
+            "INNER JOIN JpaGroupApplicantList groupApplicantList on groupApplicantList.applicantUin = applicantDigitalId.uin " +
+            "INNER JOIN groupApplicantList.applicantGroup applicantGroup " +
+            "INNER JOIN applicantGroup.groupLeader groupLeader " +
+            "INNER JOIN groupLeader.digitalIds groupLeaderDigitalId " +
+            "INNER JOIN applicantGroup.companyRitualSeason companyRitualSeason " +
+            "INNER JOIN companyRitualSeason.ritualSeason ritualSeason " +
+            "INNER JOIN companyRitualSeason.company company " +
+            "WHERE applicantDigitalId.uin =:uin "
+    )
+    ApplicantStaffVO findApplicantRitualByUin(@Param("uin") String uin);
 }
