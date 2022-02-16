e79088932c646f8ab6709740bad502624ae79a76
f.messaoudi <firas.messaoudi@sofrecom.com>
2022-02-16 17:26:12 +0100

    HAJ-1557: Admin portal - dashboard - current season statistics - age number value not visible

--- shj-admin-portal-web-fe/src/app/_shared/components/doughnut-chart/doughnut-chart.component.ts
+++ shj-admin-portal-web-fe/src/app/_shared/components/doughnut-chart/doughnut-chart.component.ts
@@ -3,6 +3,7 @@ import {ChartsConfig} from '@pages/dashboard/charts.config';
 import {CountVo} from '@app/_shared/model/countVo.model';
 import {ChartOptions, ChartType} from 'chart.js';
 import {Label, PluginServiceGlobalRegistrationAndOptions, SingleDataSet} from 'ng2-charts';
+import {I18nService} from "@dcc-commons-ng/services";
 
 @Component({
   selector: 'app-doughnut-chart',
@@ -31,12 +32,12 @@ export class DoughnutChartComponent implements OnInit, OnChanges {
   };
   public doughnutChartPlugins: PluginServiceGlobalRegistrationAndOptions[];
 
-  constructor() {
+  constructor(private i18nService: I18nService) {
   }
 
   ngOnInit() {
     this.doughnutChartLabels = new Array(this.data.length).fill('')
-      .map((v: any, i: number) => this.data[i].label + ": %" + this.data[i].percentage);
+      .map((v: any, i: number) => this.i18nService.language.startsWith('en') ? this.data[i].label + ": " + this.data[i].percentage + "%" : this.data[i].label + ": %" + this.data[i].percentage);
     this.doughnutChartData = new Array(this.data.length).fill('')
       .map((v: any, i: number) => this.data[i].count);
 
