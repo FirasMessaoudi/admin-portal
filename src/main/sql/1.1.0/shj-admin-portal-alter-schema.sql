2629e3df8926e3c44f05a653f119f1e478ce63dd
f.messaoudi <firas.messaoudi@sofrecom.com>
2022-02-21 17:34:39 +0100

    HAJ-1506: update scripts

--- src/main/sql/1.1.0/shj-admin-portal-alter-schema.sql
+++ src/main/sql/1.1.0/shj-admin-portal-alter-schema.sql
@@ -1292,9 +1292,12 @@ GO
 if not exists(select * from sys.tables where name = 'shc_applicant_mobile_log_time')
 CREATE TABLE shc_applicant_mobile_audit_log
 (
-    id             int          NOT NULL PRIMARY KEY IDENTITY (1,1),
-    user_id_number varchar(45)  NOT NULL,
+    id             int         NOT NULL PRIMARY KEY IDENTITY (1,1),
+    user_id_number varchar(45) NOT NULL,
     event          varchar(255) NULL,
     event_date     smalldatetime DEFAULT CURRENT_TIMESTAMP,
 );
 GO
+
+alter table shc_portal.shc_applicant_chat_contact alter column applicant_ritual_id int null
+go
