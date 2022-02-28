716bf8f7976cbc7ea84ffd31d786161a02aadea5
Slim Ben Hadj <slim.benhadj@sofrecom.com>
2022-02-28 08:59:59 +0100

    Fix: update queries after renaming user_id column in shc_mobile_audit_log table HAJ-1456 HAJ-1457

--- shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/MobileAuditLogRepository.java
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/MobileAuditLogRepository.java
@@ -22,7 +22,7 @@ public interface MobileAuditLogRepository extends JpaRepository<JpaMobileAuditLo
 
     @Query("SELECT NEW com.elm.shj.admin.portal.orm.entity.CountVo('', FUNCTION('DAY',mal.eventDate),COUNT(DISTINCT a),'') " +
             "FROM JpaApplicant a JOIN JpaApplicantDigitalId adi ON adi.applicantId = a.id " +
-            "JOIN JpaMobileAuditLog mal ON mal.userIdNumber = adi.uin JOIN a.rituals ar JOIN ar.applicantPackage ap " +
+            "JOIN JpaMobileAuditLog mal ON mal.userId = adi.uin JOIN a.rituals ar JOIN ar.applicantPackage ap " +
             "JOIN ap.ritualPackage rp JOIN rp.companyRitualSeason crs JOIN crs.ritualSeason rs " +
             "WHERE rs.seasonYear= :seasonYear AND rs.ritualTypeCode IN (:ritualTypeCodeList) AND mal.eventDate >= :currentDate " +
             "GROUP BY FUNCTION('DAY', mal.eventDate) ORDER BY FUNCTION('DAY', mal.eventDate)")
@@ -31,7 +31,7 @@ public interface MobileAuditLogRepository extends JpaRepository<JpaMobileAuditLo
                                          @Param("currentDate") Date currentDate);
 
     @Query("SELECT COUNT(DISTINCT a) FROM JpaApplicant a JOIN JpaApplicantDigitalId adi ON adi.applicantId = a.id " +
-            "JOIN JpaMobileAuditLog mal ON mal.userIdNumber = adi.uin JOIN a.rituals ar JOIN ar.applicantPackage ap " +
+            "JOIN JpaMobileAuditLog mal ON mal.userId = adi.uin JOIN a.rituals ar JOIN ar.applicantPackage ap " +
             "JOIN ap.ritualPackage rp JOIN rp.companyRitualSeason crs JOIN crs.ritualSeason rs " +
             "WHERE rs.seasonYear = :seasonYear AND (a.dateOfBirthGregorian BETWEEN :to AND :from) AND rs.ritualTypeCode IN (:ritualTypeCodeList)")
     long countMobileAppUsersByAgeRange(@Param("from") Date from,
