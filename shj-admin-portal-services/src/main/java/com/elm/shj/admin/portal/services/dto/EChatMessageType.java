86b791d1ecf900e19f0fd4d8defda8d69c9e5372
salzoubi <salzoubi@elm.sa>
2022-03-07 09:01:36 +0300

    HAJ-1607 add the message types lookup data, and change the column name and type from (type_id-int) to (type-varchar)

---
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/dto/EChatMessageType.java
@@ -0,0 +1,14 @@
+/*
+ *  Copyright (c) 2022 ELM. All rights reserved.
+ */
+package com.elm.shj.admin.portal.services.dto;
+
+/**
+ * Enum for chat messages types
+ *
+ * @author salzoubi
+ * @since 1.1.0
+ */
+public enum EChatMessageType {
+    TEXT, PHOTO, VIDEO,AUDIO,LOCATION;
+}
