4c843af5431d8df8a81023362f3e992c3544968b
n.sidhom <naheedSidhom0982648$3%@>
2022-04-21 09:44:16 +0200

    HAJ-1 : Update URL

--- shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/ws/SupplicationsWsController.java
+++ shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/ws/SupplicationsWsController.java
@@ -31,8 +31,8 @@ public class SupplicationsWsController {
      * @param type the type of the supplication
      * @return the supplications
      */
-    //TODO: Don't use camel case name in URI findSupplications and remove findSupplications as supplications is already in the URI.
-    @GetMapping("/findSupplications/{type}")
+
+    @GetMapping("/find/{type}")
     public ResponseEntity<WsResponse<?>> findSupplicationsByType(@PathVariable("type") String type) {
         return ResponseEntity.ok(WsResponse.builder().status(WsResponse.EWsResponseStatus.SUCCESS.getCode())
                 .body(supplicationsService.findSupplicationsListByType(type)).build());
