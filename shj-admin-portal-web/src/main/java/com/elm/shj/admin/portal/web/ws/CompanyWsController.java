1845a2f735231a044fad0509323bdf70750b6771
salzoubi <salzoubi@elm.sa>
2022-06-02 16:58:50 +0300

    HAJ-2233 add move companyController to ws package

--- shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/ws/CompanyWsController.java
+++ shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/ws/CompanyWsController.java
@@ -1,12 +1,14 @@
 /*
  * Copyright (c) 2022 ELM. All rights reserved.
  */
-package com.elm.shj.admin.portal.web.admin;
+package com.elm.shj.admin.portal.web.ws;
 
 import com.elm.shj.admin.portal.web.navigation.Navigation;
+import com.elm.shj.admin.portal.web.security.jwt.JwtTokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ahmad flaifel
  * @since 1.1.0
  */
+@CrossOrigin(
+        originPatterns = "*",
+        maxAge = 3600,
+        exposedHeaders = {"Authorization", JwtTokenService.CALLER_TYPE_HEADER_NAME, JwtTokenService.TOKEN_COOKIE_NAME},
+        allowCredentials = "true"
+)
+@Slf4j
 @RestController
 @RequestMapping(Navigation.API_COMPANY)
-@Slf4j
-@RequiredArgsConstructor(onConstructor_ = {@Autowired})
-public class CompanyController {
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CompanyWsController {
 
 
 }
