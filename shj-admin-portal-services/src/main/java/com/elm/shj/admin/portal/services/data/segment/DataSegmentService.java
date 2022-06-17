8b3b329ff094881de9c42c531e94332eb65d804b
rimtiaz <rimtiaz@elm.sa>
2022-06-17 17:18:06 +0300

    HAJ-1 remove applicant group main data segment from upload and listing

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/data/segment/DataSegmentService.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/data/segment/DataSegmentService.java
@@ -76,6 +76,6 @@ public class DataSegmentService extends GenericService<JpaDataSegment, DataSegme
     }
 
     public List<DataSegmentDto> findCommandSegments() {
-        return mapList(dataSegmentRepository.findSegments(Arrays.asList(1L, 2L, 3L,4L, 5L, 6L,7L, 8L, 9L,10L)));
+        return mapList(dataSegmentRepository.findSegments(Arrays.asList(1L, 2L, 3L,4L, 5L, 6L,7L, 8L, 9L)));
     }
 }
