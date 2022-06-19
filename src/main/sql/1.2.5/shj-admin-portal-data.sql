dad8afb1568b1bd43b69a155a6c948027d2a05b3
rimtiaz <rimtiaz@elm.sa>
2022-06-19 14:53:39 +0300

    HAJ-1 updated configuration for override staff

--- src/main/sql/1.2.5/shj-admin-portal-data.sql
+++ src/main/sql/1.2.5/shj-admin-portal-data.sql
@@ -44,4 +44,7 @@ values ('99999923', (select cr.id from shc_portal.shc_company_ritual_season cr, 
 insert into shc_portal.shc_ritual_package (reference_number, company_ritual_season_id, package_type_code, start_date,
                                            end_date)
 values ('99999924', (select cr.id from shc_portal.shc_company_ritual_season cr, shc_portal.shc_company c where cr.ritual_season_id = 3 and c.code='9_ESTABLISHMENT' and c.id=cr.company_id), 'NORMAL', CONVERT (date, '06-01-2022', 110), CONVERT (date, '07-31-2022', 110))
-    GO
+GO
+
+UPDATE shc_portal.shc_config set conf_value = 'true' where conf_key = 'data.request.company.staff.override';
+GO
