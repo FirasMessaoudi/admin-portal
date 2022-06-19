910c7c0a535844e968362cb28bc98ad23ac11a46
salzoubi <salzoubi@elm.sa>
2022-06-19 16:55:47 +0300

    HAJ-1 add group ritual steps edit mode values

--- shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/ws/IntegrationWsController.java
+++ shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/ws/IntegrationWsController.java
@@ -1347,10 +1347,10 @@ public class IntegrationWsController {
                 .body(list).build());
     }
 
-    @PostMapping("/group/ritual-step/update/{groupId}/{stepCode}/{hijriDate}/{time}")
-    public ResponseEntity<WsResponse<?>> updateGroupRitualStep(@PathVariable long groupId, @PathVariable String stepCode, @PathVariable long hijriDate, @PathVariable String time) {
+    @PostMapping("/group/ritual-step/update")
+    public ResponseEntity<WsResponse<?>> updateGroupRitualStep(@RequestBody UpdateCompanyRitualStepCmd updateCompanyRitualStepCmd) {
         log.info("updateGroupRitualStep start");
-        boolean updated = companyRitualStepService.updateGroupRitualStep(groupId,stepCode,hijriDate,time);
+        boolean updated = companyRitualStepService.updateGroupRitualStep(updateCompanyRitualStepCmd.getGroupId(),updateCompanyRitualStepCmd.getStepCode(),updateCompanyRitualStepCmd.getHijriDate(),updateCompanyRitualStepCmd.getTime());
         return ResponseEntity.ok(WsResponse.builder().status(WsResponse.EWsResponseStatus.SUCCESS.getCode()).body(updated).build());
     }
 
