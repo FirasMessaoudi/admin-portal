fa9c0c31a0eff8026c01b172ea68efe460a2e93f
jaafer.jarray <jaafer.jarray@sofrecom.com>
2022-06-20 10:33:40 +0100

    HAJ-2178: register and login with id number and passport

--- src/main/sql/1.2.5/shj-admin-portal-alter-schema.sql
+++ src/main/sql/1.2.5/shj-admin-portal-alter-schema.sql
@@ -27,3 +27,7 @@ GO
 
 ALTER TABLE shc_portal.shc_applicant_package_housing
     add site_bed_number nvarchar(30) null
+GO
+
+ALTER TABLE shc_portal.shc_applicant ALTER COLUMN date_of_birth_gregorian date NULL;
+GO
