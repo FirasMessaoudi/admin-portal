8b29f986023c037213f4187f306d8d2b5b289b6c
aflaifel <aflaifel@elm.sa>
2022-06-25 06:28:53 +0300

    show group leader for the organizer HAJ-1

--- shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/ws/IntegrationWsController.java
+++ shj-admin-portal-web/src/main/java/com/elm/shj/admin/portal/web/ws/IntegrationWsController.java
@@ -479,7 +479,7 @@ public class IntegrationWsController {
     public ResponseEntity<WsResponse<?>> findOrganizerApplicantGroupLeader(@PathVariable String uin) {
         log.debug("Handler for {}", "Find company employee by uin and season ");
         ApplicantRitualPackageVo applicantPackage = applicantPackageService.findLatestApplicantRitualPackage(Long.parseLong(uin));
-        return ResponseEntity.ok(WsResponse.builder().status(WsResponse.EWsResponseStatus.SUCCESS.getCode()).body(companyStaffService.findRelatedEmployeesByApplicantUinAndSeasonId(uin, applicantPackage.getCompanyRitualSeasonId())).build());
+        return ResponseEntity.ok(WsResponse.builder().status(WsResponse.EWsResponseStatus.SUCCESS.getCode()).body(companyStaffService.findGroupLeaderByApplicantUin(uin, applicantPackage.getCompanyRitualSeasonId())).build());
     }
 
     // End Organizer applicant main data, details, health and group leader
