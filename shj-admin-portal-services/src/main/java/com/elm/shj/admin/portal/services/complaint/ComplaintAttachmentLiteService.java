a1fe8f760d73e5bfb066ea290b2638dc5d4ad979
oalamoud <oalamous@elm.sa>
2022-06-26 20:58:29 +0300

    HAJ-1 - Fix Add Complaint

---
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/complaint/ComplaintAttachmentLiteService.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021 ELM. All rights reserved.
+ */
+package com.elm.shj.admin.portal.services.complaint;
+
+import com.elm.shj.admin.portal.orm.entity.JpaComplaintAttachmentLite;
+import com.elm.shj.admin.portal.services.dto.ComplaintAttachmentLiteDto;
+import com.elm.shj.admin.portal.services.generic.GenericService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+/**
+ * Service handling Applicant Complaint Attachment operations
+ *
+ * @author othman alamoud
+ * @since 1.2.6
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ComplaintAttachmentLiteService extends GenericService<JpaComplaintAttachmentLite, ComplaintAttachmentLiteDto, Long> {
+
+
+    public ComplaintAttachmentLiteDto save(ComplaintAttachmentLiteDto dto){
+        return getMapper().fromEntity(getRepository().save(getMapper().toEntity(dto, mappingContext)), mappingContext);
+    }
+}
+
