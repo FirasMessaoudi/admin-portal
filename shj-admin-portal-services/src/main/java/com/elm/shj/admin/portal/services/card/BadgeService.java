4646f1f77e38a1e6080902a57104ad642606cac3
f.messaoudi <firas.messaoudi@sofrecom.com>
2022-06-27 13:01:44 +0100

    HAJ-1: change badge dimensions and extension to bmp

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/card/BadgeService.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/card/BadgeService.java
@@ -118,7 +118,7 @@ public class BadgeService {
         String nationalityAr = arabicCountryLookup == null ? "" : arabicCountryLookup.getLabel();
         NationalityLookupDto englishCountryLookup = mainLangCountryLookupList.stream().filter(countryLookup -> countryLookup.getLang().equals("en")).findFirst().orElse(null);
         String nationalityEn = englishCountryLookup == null ? "" : englishCountryLookup.getLabel();
-        BufferedImage badgeImage = new BufferedImage(BADGE_WIDTH, BADGE_HEIGHT, BufferedImage.TYPE_4BYTE_ABGR);
+        BufferedImage badgeImage = new BufferedImage(BADGE_WIDTH, BADGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = badgeImage.createGraphics();
 
         g2d.setColor(new Color(0xFFF7F7F7));
