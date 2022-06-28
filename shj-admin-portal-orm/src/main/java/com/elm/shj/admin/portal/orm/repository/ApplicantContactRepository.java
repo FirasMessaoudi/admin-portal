d298982204d3ae8c7ce1b8b716bff75bb19b9fe3
salzoubi <salzoubi@elm.sa>
2022-06-28 17:27:58 +0300

    HAJ-1 remove update country code when update mobile number

--- shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/ApplicantContactRepository.java
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/ApplicantContactRepository.java
@@ -21,14 +21,14 @@ public interface ApplicantContactRepository extends JpaRepository<JpaApplicantCo
     @Modifying
     @Query("update JpaApplicantContact contact set  contact.email = :email, " +
             "contact.intlMobileNumber =:intlMobileNumber, contact.updateDate = CURRENT_TIMESTAMP where contact.applicant.id =:applicantId")
-    int updateContactIntlNumber(@Param("email") String email, @Param("countryCode") String countryCode,
+    int updateContactIntlNumber(@Param("email") String email,
                                 @Param("intlMobileNumber") String intlMobileNumber, @Param("applicantId") long applicantId);
 
 
     @Modifying
     @Query("update JpaApplicantContact contact set  contact.email = :email, " +
             "contact.localMobileNumber =:localMobileNumber, contact.updateDate = CURRENT_TIMESTAMP where contact.applicant.id =:applicantId")
-    int updateContactLocalNumber(@Param("email") String email, @Param("countryCode") String countryCode,
+    int updateContactLocalNumber(@Param("email") String email,
                                  @Param("localMobileNumber") String localMobileNumber, @Param("applicantId") long applicantId);
     List<JpaApplicantContact> findAllByApplicantId(Long id);
 }
