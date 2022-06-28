b31e9f426f64e6069886ded3b5613036b6a7f0f0
aflaifel <aflaifel@elm.sa>
2022-06-28 10:08:11 +0300

    update group reference number to accept Arabic HAJ-1

--- src/main/sql/1.3.3/shj-admin-portal-alter-schema.sql
+++ src/main/sql/1.3.3/shj-admin-portal-alter-schema.sql
@@ -1,5 +1,7 @@
 USE shc_portal
 GO
-ALTER TABLE shc_portal.shc_notification_template
-    ADD company_code varchar(100)
+ALTER TABLE shc_portal.shc_notification_template ADD company_code varchar(100);
+GO
+
+ALTER TABLE shc_portal.shc_applicant_group ALTER COLUMN reference_number nvarchar(100);
 GO
