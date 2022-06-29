071ea0275dcbdb9ce70ad3acee17437033e4e4f2
salzoubi <salzoubi@elm.sa>
2022-06-29 12:58:35 +0300

    HAJ-1

--- shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/CompanyStaffRepository.java
+++ shj-admin-portal-orm/src/main/java/com/elm/shj/admin/portal/orm/repository/CompanyStaffRepository.java
@@ -114,13 +114,13 @@ public interface CompanyStaffRepository extends JpaRepository<JpaCompanyStaff, L
     String findGroupLeaderMobileNumberByApplicantUin(@Param("applicantUin") String applicantUin);
 
     @Modifying
-    @Query("update JpaCompanyStaff staff set staff.countryCode = :countryCode, staff.countryPhonePrefix = :countryPhonePrefix, staff.email = :email, " +
+    @Query("update JpaCompanyStaff staff set  staff.countryPhonePrefix = :countryPhonePrefix, staff.email = :email, " +
             "staff.mobileNumberIntl =:intlMobileNumber, staff.registered = TRUE, staff.updateDate = CURRENT_TIMESTAMP where staff.id =:staffId")
     int updateCompanyStaffIntlNumber(@Param("email") String email, @Param("countryPhonePrefix") String countryPhonePrefix, @Param("intlMobileNumber") String intlMobileNumber, @Param("staffId") long staffId);
 
 
     @Modifying
-    @Query("update JpaCompanyStaff staff set staff.countryCode = :countryCode,  staff.email = :email, " +
+    @Query("update JpaCompanyStaff staff set  staff.email = :email, " +
             "staff.mobileNumber =:localMobileNumber, staff.registered = TRUE, staff.updateDate = CURRENT_TIMESTAMP where staff.id =:staffId")
     int updateCompanyStaffLocalNumber(@Param("email") String email,  @Param("localMobileNumber") String localMobileNumber, @Param("staffId") long staffId);
 
