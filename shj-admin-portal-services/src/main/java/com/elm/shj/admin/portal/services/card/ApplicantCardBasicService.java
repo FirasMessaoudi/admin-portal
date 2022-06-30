56ddd494e5a43c5f12e1d5d24d6dd5cfed5a434c
aflaifel <aflaifel@elm.sa>
2022-06-30 21:48:47 +0300

    handle digital id and card for deleted applicant and release 1.3.4-SNAPSHOT HAJ-1

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/card/ApplicantCardBasicService.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/card/ApplicantCardBasicService.java
@@ -4,12 +4,15 @@
 package com.elm.shj.admin.portal.services.card;
 
 import com.elm.shj.admin.portal.orm.entity.JpaApplicantCardBasic;
+import com.elm.shj.admin.portal.orm.repository.ApplicantCardBasicRepository;
 import com.elm.shj.admin.portal.services.dto.ApplicantCardBasicDto;
+import com.elm.shj.admin.portal.services.dto.ECardStatus;
 import com.elm.shj.admin.portal.services.generic.GenericService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Service handling applicant card
@@ -22,4 +25,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class ApplicantCardBasicService extends GenericService<JpaApplicantCardBasic, ApplicantCardBasicDto, Long> {
 
+    private final ApplicantCardBasicRepository applicantCardBasicRepository;
+
+    @Transactional
+    public void deleteAllApplicantCards(Long applicantId) {
+        applicantCardBasicRepository.deleteAllApplicantCards(applicantId, ECardStatus.CANCELLED.name());
+    }
 }
