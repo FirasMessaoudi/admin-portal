3ff5741867946d828a0e7241451e0b2ebc418241
oalamoud <oalamous@elm.sa>
2022-07-02 00:47:36 +0300

    HAJ-1 - Fix issues

--- shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/incident/ApplicantIncidentService.java
+++ shj-admin-portal-services/src/main/java/com/elm/shj/admin/portal/services/incident/ApplicantIncidentService.java
@@ -152,21 +152,6 @@ public class ApplicantIncidentService extends GenericService<JpaApplicantInciden
         return mapList(applicantIncidentRepository.findByApplicantRitualId(applicantRitualId));
     }
 
-
-    /**
-     * fetches the original file of the data request
-     *
-     * @param incidentAttachmentId applicant Incident Attachment Id
-     * @return the attachment of the applicant incident
-     */
-    public Resource downloadApplicantIncidentAttachment(long incidentAttachmentId) throws Exception {
-        Optional<JpaIncidentAttachment> incidentAttachment = incidentAttachmentRepository.findById(incidentAttachmentId);
-        if (!incidentAttachment.isPresent()) {
-            return null;
-        }
-        return sftpService.downloadFile(incidentAttachment.get().getFilePath(), APPLICANT_INCIDENTS_CONFIG_PROPERTIES);
-    }
-
     /**
      * Updates applicant incident
      *
@@ -228,8 +213,8 @@ public class ApplicantIncidentService extends GenericService<JpaApplicantInciden
             throw new NotFoundException("no Template found for  " + closeIncidentTemplateName);
         }
 
-        ApplicantIncidentDto applicantIncident = findById(incidentId);
-        String uin = applicantIncident.getApplicantRitual().getApplicant().getDigitalIds().get(0).getUin();
+        com.elm.shj.admin.portal.orm.entity.ApplicantComplaintVo applicantIncident = applicantIncidentLiteRepository.findOneLite(incidentId);
+        String uin = applicantIncident.getApplicantRitual().getApplicant().getUin();
         String preferredLanguage = applicantIncident.getApplicantRitual().getApplicant().getPreferredLanguage();
         notificationRequestService.sendIncidentNotification(notificationTemplate.get(), uin, preferredLanguage);
     }
