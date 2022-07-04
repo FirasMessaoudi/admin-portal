064ae27d6439c2ee6de889d36f266f9f0bdcf2e7
adhaoui <adhaoui@elm.sa>
2022-07-04 13:19:53 +0300

    HAJ-583 adding changes for BI Dashboard

--- src/main/sql/1.3.6/shj-admin-portal-data.sql
+++ src/main/sql/1.3.6/shj-admin-portal-data.sql
@@ -2,6 +2,7 @@ USE shc_portal
 GO
 
 INSERT INTO shc_portal.shc_config(conf_key, conf_value) VALUES('bi.server.url','10.33.191.24');
+INSERT INTO shc_portal.shc_config(conf_key, conf_value) VALUES('bi.server.domain','10.33.191.24');
 INSERT INTO shc_portal.shc_config(conf_key, conf_value) VALUES('bi.server.client.id','hajj');
 INSERT INTO shc_portal.shc_config(conf_key, conf_value) VALUES('bi.dashboard.uri','views/SMARTGATEHAJJDASHBOARDS/HAJJ1433');
 
