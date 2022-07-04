75bce10ae80958a8c5334621ceb81de677b8ee11
Noor Nawaz <nnawaz@elm.sa>
2022-07-04 14:18:36 +0300

    HAJ-1 script moved to new version

--- src/main/sql/1.3.6/shj-admin-portal-data.sql
+++ src/main/sql/1.3.6/shj-admin-portal-data.sql
@@ -6,6 +6,4 @@ INSERT INTO shc_portal.shc_config(conf_key, conf_value) VALUES('bi.server.domain
 INSERT INTO shc_portal.shc_config(conf_key, conf_value) VALUES('bi.server.client.id','hajj');
 INSERT INTO shc_portal.shc_config(conf_key, conf_value) VALUES('bi.dashboard.uri','views/SMARTGATEHAJJDASHBOARDS/HAJJ1433');
 
-INSERT INTO shc_portal.shc_config (conf_key, conf_value) VALUES ('scheduler.notification.template.processing.active.nodes', '127.0.0.1,localhost');
-INSERT INTO shc_portal.shc_config (conf_key, conf_value) VALUES ('scheduler.notification.processing.active.nodes', '127.0.0.1,localhost');
 GO
--- src/main/sql/1.3.7/shj-admin-portal-data.sql
+++ src/main/sql/1.3.7/shj-admin-portal-data.sql
@@ -1,4 +1,6 @@
 USE shc_portal
 GO
 
+INSERT INTO shc_portal.shc_config (conf_key, conf_value) VALUES ('scheduler.notification.template.processing.active.nodes', '127.0.0.1,localhost');
+INSERT INTO shc_portal.shc_config (conf_key, conf_value) VALUES ('scheduler.notification.processing.active.nodes', '127.0.0.1,localhost');
 GO
