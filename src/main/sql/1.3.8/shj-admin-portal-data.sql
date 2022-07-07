c9a2aa7ff0a1d778d54fdb533d5ffb511e1dfd71
Omar Adel <oabdelatty@elm.sa>
2022-07-07 14:29:06 +0300

    HAJ-1 Adding chatbot french data

--- src/main/sql/1.3.8/shj-admin-portal-data.sql
+++ src/main/sql/1.3.8/shj-admin-portal-data.sql
@@ -87,5 +87,89 @@ INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VAL
 INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (113, 'SEPCIAL_NEEDS_SERVICES', 'en', 'What services are available for persons with special needs?', 'SPECIAL_NEEDS');
 INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (114, 'SEPCIAL_NEEDS_SERVICES_RESPONSE', 'en','You can contact the General Presidency of Haramain in this regard.','SEPCIAL_NEEDS_SERVICES');
 
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label) VALUES (115, 'HAJJ_RITUAL', 'fr','A propos du Hajj');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label) VALUES (116, 'LOCATIONS', 'fr','Lieux importants ');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label) VALUES (117, 'CONTACTS', 'fr','Numéros de contact importants');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label) VALUES (118, 'SMART_CARD', 'fr','Carte à puce');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label) VALUES (119, 'SPECIAL_NEEDS', 'fr','Besoins spécifiques');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (120, 'SLAUGHTER_PROCESS','fr','Quand l''abattage peut-il être confirmé ? Comme lorsque la personne requérant le rituel est un des pèlerins, de sorte que l''accomplissement de son rituel est lié à sa sortie de l''état d''Ihraam au Hajj ou le sacrifiant qui n''est pas un pèlerin ?', 'HAJJ_RITUAL');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (121, 'SLAUGHTER_PROCESS_RESPONSE','fr', 'Après l''achat et dès que le montant est déduit du compte du client et que sa réception sur le compte du Projet est confirmée, ou que le client obtient le coupon ou le bon de rituel, le client est donc considéré comme ayant confié au Projet l''accomplissement du rituel en son nom ; et ainsi, il est déchargé de l''accomplissement du rituel, en notant que dans le cas où l''achat et le paiement ont été effectués par le site web, un message sera envoyé pour informer le client de l''exécution de sa demande (l''achèvement de l''abattage).','SLAUGHTER_PROCESS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (122, 'ELECTRONIC_PATH', 'fr', 'La voie électronique permet-elle d''acheter des Hady et des Adahi (animaux sacrifiés) ?', 'HAJJ_RITUAL');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (123, 'ELECTRONIC_PATH_RESPONSE', 'fr','Oui, ce service est disponible.','ELECTRONIC_PATH');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (124, 'HAJJ_APPLICATION', 'fr','Quand débute le dépôt des demandes pour la saison du Hajj 1443 AH ?', 'HAJJ_RITUAL');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (125, 'HAJJ_APPLICATION_RESPONSE', 'fr','L''annonce de la date d''ouverture des inscriptions pour le Hadj 1443 AH a commencé sur le site web du ministère du Hadj et de la Omra et sur ses canaux officiels le vendredi 4/11/1443 AH (Le 3 Juin 2022)', 'HAJJ_APPLICATION');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (126, 'HAJJ_AGE', 'fr','Quel est l''âge autorisé pour accomplir le Hajj ?', 'HAJJ_RITUAL');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (127, 'HAJJ_AGE_RESPONSE', 'fr','Il n''y a aucune contre-indication à accompagner des enfants pendant le Hajj de 1443, à condition qu''ils soient vaccinés.', 'HAJJ_AGE');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (128, 'INTERNAL_HAJJ', 'fr','Quels sont les dates de début et de fin de la demande pour les campagnes internes du Hajj ?', 'HAJJ_RITUAL');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (129, 'INTERNAL_HAJJ_RESPONSE', 'fr','Les inscriptions commencent le 4 de Dhu Al-Qaedah (le 3 Juin) et se terminent le 13 de Dhu Al-Qaedah (le 12 Juin).', 'INTERNAL_HAJJ');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (130, 'HAJJ_INQUIRY', 'fr','Comment se renseigner sur l''éligibilité au Hajj ?', 'HAJJ_RITUAL');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (131, 'HAJJ_INQUIRY_RESPONSE', 'fr','Pour se renseigner sur l''éligibilité au Hajj, il suffit de se rendre sur la plate-forme Absher, de sélectionner " e-inquiries ", de se renseigner sur l''éligibilité au Hajj, puis de saisir le numéro d''identification et le code visuel, et l''éligibilité apparaîtra.', 'HAJJ_INQUIRY');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (132, 'GULF_NATIONALS', 'fr','Citoyens du CCG qui se trouvent dans le Royaume mais ne sont pas résidents et souhaitent accomplir le Hajj national', 'HAJJ_RITUAL');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (133, 'GULF_NATIONALS_RESPONSE', 'fr','Un citoyen du CCG qui réside dans le Royaume peut s''inscrire dans la voie électronique en utilisant le numéro de saisie qu''il peut obtenir auprès du département des passeports du CCG ""*. Un citoyen du CCG qui ne réside pas dans le Royaume ne peut pas s''enregistrer dans la voie électronique destinée aux pèlerins nationaux. Il doit s''enregistrer dans son pays conformément à la réglementation de celui-ci. ', 'GULF_NATIONALS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (134, 'MADINAH_CARE_CENTERS', 'fr','Centres de soins de Médine','LOCATIONS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (135, 'MADINAH_CARE_CENTERS_RESPONSE', 'fr','Centre de l''est de la mosquée du Prophète Adresse : King Faisal Road, 1st Ring Road, Al-Haram Horaires d''ouverture : 7h-21h Aéroport international Prince Mohammad bin Abdulaziz Adresse : Aéroport international Prince Mohammad bin Abdulaziz Horaires d''ouverture : 8h - 22h','MADINAH_CARE_CENTERS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (136, 'JEDDAH_CARE_CENTERS', 'fr','Centres de soins de Djeddah','LOCATIONS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (137, 'JEDDAH_CARE_CENTERS_RESPONSE', 'fr','Aéroport international Roi Abdulaziz à Djeddah Adresse : King Abdulaziz International Airport Terminal No. 1','JEDDAH_CARE_CENTERS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (138, 'MAKKAH_CARE_CENTERS', 'fr','Centres de soins de La Mecque','LOCATIONS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (139, 'MAKKAH_CARE_CENTERS_RESPONSE', 'fr','Succursale d''Al-Hujun : Madinah Road, Al Hujun, Makkah. Horaires d''ouverture : 8h - 22h Lien de localisation sur Google Maps : https://maps.app.goo.gl/NE1G4t6S6WvNkDVb8 Succursale de Misfalah : Adresse : Ibrahim Al Khalil Street, Misfalah, Makkah. Horaires d''ouverture : 8h - 22h Lien de localisation sur Google Maps : https://maps.app.goo.gl/NGCnEg6hCp9JTT238 Succursale de Dar Al-Tawhid Hotel / Makkah Lien de localisation sur Google Maps : https://maps.app.goo.gl/3c9iDmVRL2Zqk3n57','MAKKAH_CARE_CENTERS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (140, 'HARAM', 'fr','Al-Haram','LOCATIONS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (141, 'HARAM_RESPONSE', 'fr', 'https://goo.gl/maps/P9L6JX9VCNgcuxR3A','HARAM');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (142, 'MERCY_MOUNT', 'fr','Montagne Rahma','LOCATIONS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (143, 'MERCY_MOUNT_RESPONSE', 'fr', 'https://goo.gl/maps/TjMBJqj754QzJFg88','MERCY_MOUNT');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (144, 'ARAFAT_MOUNT', 'fr','Montagne Arafat','LOCATIONS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (145, 'ARAFAT_MOUNT_RESPONSE', 'fr', 'https://goo.gl/maps/hGcK5D7uY2jcr6gP7','ARAFAT_MOUNT');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (146, 'MASJID_NABAWI', 'fr','La mosquée du Prophète','LOCATIONS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (147, 'MASJID_NABAWI_RESPONSE', 'fr', 'https://goo.gl/maps/SseRYc5NoZXXHxsh8','MASJID_NABAWI');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (148, 'FATWA_NUMBERS', 'fr','Numéros de fatwa (opinion religieux)','CONTACTS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (149, 'FATWA_NUMBERS_RESPONSE', 'fr','Numéro saoudien de fatwa 24 heures sur 24 ou numéro gratuit de fatwa Numéro gratuit 24 heures sur 24, de fatwa : 8002451000 Numéro de fatwa unifié, Makkah : 8001222100 Numéro de la Fatwa de Makkah : 8001222400 Numéro de Fatwa de la radio du Saint Coran : 0114052999 Numéro de Dar Al-Iftaa saoudien : 0114595555 Numéro de fatwa du MDHO : 8002451000 Numéro de fatwa WhatsApp (1) : 0501785536','FATWA_NUMBERS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (150, 'PRESIDENCY_MOSQUES_NUMBERS', 'fr','Présidence générale de Haramain','CONTACTS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (151, 'PRESIDENCY_MOSQUES_NUMBERS_RESPONSE', 'fr','Numéro gratuit de contact 1966','PRESIDENCY_MOSQUES_NUMBERS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (152, 'HAJJ_MINISTRY', 'fr','Ministère du Hajj et de la Omra','CONTACTS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (153, 'HAJJ_MINISTRY_RESPONSE', 'fr','Pour contacter le ministère du Hadj et de la Omra, vous pouvez joindre le centre de service clientèle du MDHO au 920002814 ou à care@haj.gov.sa, ou par le biais du compte officiel Twitter @MOHU_Care.','HAJJ_MINISTRY');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (154, 'EMERGENCY_NUMBER', 'fr','Pour les urgences et les services de sécurité','CONTACTS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (155, 'EMERGENCY_NUMBER_RESPONSE', 'fr', '911','EMERGENCY_NUMBER');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (156, 'MEDICAL_ADVICES', 'fr','Pour les consultations et les demandes de renseignements médicaux','CONTACTS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (157, 'MEDICAL_ADVICES_RESPONSE', 'fr', '937','MEDICAL_ADVICES');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (158, 'EVALUATION', 'fr', 'Évaluation', 'SMART_CARD');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (159, 'EVALUATION_RESPONSE', 'fr','Un compte-rendu quotidien et une évaluation à la fin du voyage du pèlerin peuvent être sélectionnés dans la liste des services, veuillez donc prêter attention à la remplir pour nous aider à améliorer les services. ','EVALUATION');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (160, 'COMPLAINTS', 'fr', 'Plaintes', 'SMART_CARD');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (161, 'COMPLAINTS_RESPONSE', 'fr','La plainte est déposée auprès de l''organisateur, et après une période sans résolution, la plainte est évoquée comme un rapport. ','COMPLAINTS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (162, 'INCIDENTS', 'fr', 'Rapports', 'SMART_CARD');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (163, 'INCIDENTS_RESPONSE', 'fr','Le rapport est envoyé à l''organisateur et soumis aux autorités officielles pour qu''elles prennent les mesures nécessaires. ','INCIDENTS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (164, 'CARD_COLORS', 'fr', 'Couleurs de la carte', 'SMART_CARD');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (165, 'CARD_COLORS_RESPONSE', 'fr','Les couleurs de la carte sont basées sur l''établissement organisateur du pèlerinage auquel le pèlerin est affilié, et il existe une couleur distinctive pour les pèlerins nationaux. ','CARD_COLORS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (166, 'WHEELCHAIRS_GATES', 'fr', 'Quelles sont les portes prévues pour les personnes en fauteuil roulant dans le hall d''Al-Haram ?', 'SPECIAL_NEEDS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (167, 'WHEELCHAIRS_GATES_RESPONSE', 'fr','Vous pouvez contacter la Présidence générale de Haramain à 1966 à ce sujet.','WHEELCHAIRS_GATES');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (168, 'HAJJ_CARTS', 'fr', 'Des véhicules sont-ils disponibles à l''intérieur d''Al-Haram ?', 'SPECIAL_NEEDS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (169, 'HAJJ_CARTS_RESPONSE', 'fr','La Présidence générale de Haramain met à disposition des véhicules électriques, qui peuvent être loués via l''application Tanaqol, pour effectuer la circumambulation ou la déambulation à des étages spécifiques d''Al-Haram. Veuillez contacter la Présidence de Haramain pour tout renseignement complémentaire.','HAJJ_CARTS');
+
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (170, 'SEPCIAL_NEEDS_SERVICES', 'fr', 'Quels sont les services disponibles pour les personnes ayant des besoins particuliers ?', 'SPECIAL_NEEDS');
+INSERT INTO shc_portal.shc_chatbot_item (id, code, lang, label, parent_code) VALUES (171, 'SEPCIAL_NEEDS_SERVICES_RESPONSE', 'fr','Vous pouvez contacter la Présidence générale de Haramain à ce sujet.','SEPCIAL_NEEDS_SERVICES');
+
 SET IDENTITY_INSERT shc_portal.shc_chatbot_item OFF;
 GO
